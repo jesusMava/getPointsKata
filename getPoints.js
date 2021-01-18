18bf1d214d9e89d28db0bb03590cc75edd2eff7b
jesusMava <jesusmacedo7@gmail.com>
2021-01-17 20:07:01 -0600

    test: Add test and refactor

--- getPoints.js
+++ getPoints.js
@@ -1,21 +1,22 @@
 let arr = []
 let pos = 0
 let acc = arr[0]
+let resul = []
 
-const getPoints = (acc, pos, arr) => {
+const getPoints = (resul, acc, pos, arr) => {
   if(arr.length == 0) return []
+   if(pos < arr.length - 1) {
+     return (arr[pos + 1][0] <= acc[1]) ?
+       (
+         acc = (acc[1] < arr[pos + 1][1]) ? 
+         [acc[0], arr[pos + 1][1]] : acc,
+         getPoints(resul, acc, pos + 1, arr)
+       ) :
+       getPoints([...resul, acc], acc = arr[pos + 1], pos + 1, arr)
+   }
+   return [...resul, acc]
+ }
 
-  if(pos < arr.length - 1) {
-    if(arr[pos + 1][0] <= acc[1]) {
-      if(acc[1] < arr[pos + 1][1]) {
-        acc = [acc[0], arr[pos + 1][1]]
-      }
-      return getPoints(acc, pos + 1, arr)
-    }
-  }
-  return [acc, arr[pos + 1]]
-}
-
-console.log(getPoints(acc, pos, arr))
+console.log(getPoints(resul, acc, pos, arr))
 
 export default getPoints
--- getPoints.test.js
+++ getPoints.test.js
@@ -3,33 +3,79 @@ import getPoints from './getPoints'
 let pos = 0
 let arr = [] 
 let acc = []
+let result = []
 
-test('get [[1,3],[2,4],[5,6]] equal [[1,4], [5, 6]]', () => {
+test('get [[1, 3], [2, 4], [5, 6]] equal [[1, 4], [5, 6]]', () => {
   pos = 0
-  arr = [[1,3],[2,4],[5,6]] 
+  arr = [[1, 3], [2, 4], [5, 6]] 
   acc = arr[0]
-  expect(getPoints(acc, pos, arr)).toEqual([[ 1, 4 ], [ 5, 6 ]])
+  expect(getPoints(result, acc, pos, arr)).toEqual([[ 1, 4 ], [ 5, 6 ]])
 })
 
-test('get [[-2, 2], [2,4], [4,7], [9,10]] equal [[-2,7], [9,10]]', () => {
+test('get [[-2, 2], [2, 4], [4, 7], [9, 10]] equal [[-2, 7], [9, 10]]', () => {
   pos = 0
-  arr = [[-2, 2], [2,4], [4,7], [9,10]]
+  arr = [[-2, 2], [2, 4], [4, 7], [9, 10]]
   acc = arr[0]
-  expect(getPoints(acc, pos, arr)).toEqual([[-2,7], [9,10]])
+  expect(getPoints(result, acc, pos, arr)).toEqual([[-2, 7], [9, 10]])
 })
 
-test('get [[-3, 2], [0,1], [2,4], [5,6]] equal [[-3,4], [5,6]]', () => {
-    pos = 0
-    arr = [[-3, 2], [0,1], [2,4], [5,6]]
-    acc = arr[0]
-    expect(getPoints(acc, pos, arr)).toEqual([[-3,4], [5,6]])
-  
+test('get [[-3, 2], [0, 1], [2, 4], [5, 6]] equal [[-3, 4], [5, 6]]', () => {
+  pos = 0
+  arr = [[-3, 2], [0, 1], [2, 4], [5, 6]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([ [ -3, 4 ], [ 5, 6] ])
+
 })
 
 test('get [] equal []', () => {
-    pos = 0
-    arr = []
-    acc = arr[0]
-    expect(getPoints(acc, pos, arr)).toEqual([])
-  
+  pos = 0
+  arr = []
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([])
+
+})
+
+test('get [[1, 2], [3, 4], [5, 6]] equal [[1, 2], [3, 4], [5, 6]]', () => {
+  pos = 0
+  arr =[[1, 2], [3, 4], [5, 6]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[1, 2], [3, 4], [5, 6]])
+})
+
+test('get [[0, 2], [1, 4], [5, 6], [7, 8]] equal [[0, 4], [5, 6], [7, 8]]', () => {
+  pos = 0
+  arr = [[0, 2], [1, 4], [5, 6], [7, 8]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[0, 4], [5, 6], [7, 8]])
+
+})
+
+test('get [[-13, 2], [0, -8], [2, 4], [5, 6]] equal [[-13, 4], [5, 6]]', () => {
+  pos = 0
+  arr = [[-13, 2], [0, -8], [2, 4], [5, 6]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[-13, 4], [5, 6]])
+})
+
+test('get [[-13, 2], [0, -8], [2, 4]] equal [[-13, 4]]', () => {
+  pos = 0
+  arr = [[-13, 2], [0, -8], [2, 4]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[-13, 4]])
+})
+
+test('get [[-13, 2], [0, 8], [2, 4]] equal [[-13, 8]]', () => {
+  pos = 0
+  arr = [[-13, 2], [0, 8], [2, 4]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[-13, 8]])
+
+})
+
+test('get [[1, 4], [6, 9], [15, 76]] equal [[1, 2], [3, 4], [5, 6]]', () => {
+  pos = 0
+  arr = [[1, 4], [6, 9], [15, 76]]
+  acc = arr[0]
+  expect(getPoints(result, acc, pos, arr)).toEqual([[1, 4], [6, 9], [15, 76]])
+
 })
